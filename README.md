7eb005956bbc297cf3798b6adda7839bc46b19eb
Dmitri Kunin <info@dkunin.ru>
2015-08-12 21:54:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# WakaTime module 
+
+> To check lenght of todays coding time
+
+## Install
+
+```js
+
+  npm install wakatoday
+
+```
+## Usage
+
+```js
+  
+    var getWakaTime = require('wakatoday');
+
+    getTime('<YOUR_API_KEY>'').then(console.log).catch(console.log); //6h 10m 12s
+
+```
+
+## License
+
+MIT © [Dmitri Kunin](http://dkun.in)
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var request  = require('superagent');
+var prettyMs = require('pretty-ms');
+var R        = require('ramda');
+
+var todaysDate = function(){
+  var d = new Date();
+  return (d.getMonth()+1)+'/'+d.getDate()+'/'+d.getFullYear();
+};
+
+var today = todaysDate();
+
+var summDuration = function(item, itemb){
+  return item.total_seconds + itemb.total_seconds;
+};
+
+var getFullDuration = R.compose(prettyMs, R.multiply(1000), R.reduce(summDuration, {total_seconds:0}), R.pluck('grand_total'),R.prop('data'));
+
+function getHours(apiKey){
+  console.log(apiKey);
+  return new Promise(function(resolve,reject){
+    request
+      .get('https://wakatime.com/api/v1/summary/daily')
+      .query({start:today, end: today, api_key: apiKey})
+      .end(function (error, body) {
+        if(error) {
+          reject(error);
+          return;
+        }
+        resolve(getFullDuration(body.body));
+    }); 
+  });
+}
+
+module.exports = getHours;
