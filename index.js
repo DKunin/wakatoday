04666dcdd5f9ceeff65b2eeb37e9b959dbb18b48
Dmitry Kunin <dkunin@avito.ru>
2015-12-09 08:41:20 +0300

    Updated logic for updated api, added simple test

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ var summDuration = function(item, itemb){
   return item.total_seconds + itemb.total_seconds;
 };
 
-var getFullDuration = R.compose(prettyMs, R.multiply(1000), R.reduce(summDuration, {total_seconds:0}), R.pluck('grand_total'),R.prop('data'));
+var getFullDuration = R.compose(prettyMs, R.multiply(1000), R.sum, R.pluck('duration'),R.prop('data'));
 
 function getHours(apiKey){
   return new Promise(function(resolve,reject){
     request
-      .get('https://wakatime.com/api/v1/users/current/summaries')
-      .query({start:today, end: today, api_key: apiKey})
+      .get('https://wakatime.com/api/v1/users/current/durations')
+      .query({date:today, api_key: apiKey})
       .end(function (error, body) {
         if(error) {
           reject(error);
